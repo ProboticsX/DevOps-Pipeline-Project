9d2ac393ed64d8824ba9b77c862c6d2100ac1d3b
jayrshah98 <jayrajivshah98@gmail.com>
2022-03-07 16:52:05 -0500

    Added build.js

---
+++ commands/build.js
@@ -0,0 +1,16 @@
+const chalk = require('chalk');
+const path = require('path');
+const builder = require('../lib/builder');
+
+exports.command = 'build <job_name> <build_yaml>';
+exports.desc = 'Prepare tool';
+exports.builder = yargs => {
+    yargs.options({
+    });
+};
+
+exports.handler = async argv => {
+    const { job_name, build_yaml } = argv;
+    console.log(chalk.green(job_name, build_yaml));
+    //await builder.buildAndProvisionVM();
+};
