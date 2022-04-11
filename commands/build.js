80b03c5fae364aba8dc732dfb09ac35312f2e2f7
Jay <jshah7@ncsu.edu>
2022-04-11 18:49:07 -0400

    Changes made in M1

--- commands/build.js
+++ commands/build.js
@@ -1,16 +1,110 @@
-const chalk = require('chalk');
-const path = require('path');
-const builder = require('../lib/builder');
 
-exports.command = 'build <job_name> <build_yaml>';
-exports.desc = 'Prepare tool';
-exports.builder = yargs => {
-    yargs.options({
-    });
-};
+const fs = require('fs');
+const yaml = require('js-yaml');
+const child_process = require("child_process");
+const execSync = require('child_process').execSync;
+exports.command = 'build <job_name> <build_yml>';
+const os = require('os');
+const { options } = require("yargs");
+require('dotenv').config();
+
+
+class Setup{
+    // Executing the commands in VM via ssh
+    async sshIntoVM(command,processor) {
+        let configFile = fs.readFileSync('VM_Info.json', (err) => {
+            if (err) {
+                console.error(err);
+                return;
+            }
+        });
+      
+        
+    let vm_ssh = null
+    configFile = JSON.parse(configFile);
+    if(processor == "Intel/Amd64")
+    {
+        vm_ssh = `ssh -i "${configFile.private_key}" ${configFile.user}@${configFile.hostname} -p ${configFile.port} -o StrictHostKeyChecking=no `;
+    }
+    else
+    {
+        vm_ssh = `ssh -i "${configFile.identifyFile}" -p ${configFile.port} -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null ${configFile.User}@${configFile.HostName}`;
+    }
+    
+     
+    let token = process.env.Git_Token
+
+    let passwd = process.env.mySQL_passwd
+    command = command.replace('passwd', passwd);
+
+    let microServiceUrl = `https://${token}@github.com/chrisparnin/checkbox.io-micro-preview`
+    command = command.replace('microServiceGitUrl', microServiceUrl);
+
+    let snapshotGitUrl = `https://${token}@github.com/ruttabega/screenshot`
+    command = command.replace('snapshotGitUrl', snapshotGitUrl);
+
+    console.log('vm ssh :::',vm_ssh,':::',command);
+        execSync(`${vm_ssh} "${command}"`, {stdio: ['inherit', 'inherit', 'inherit']});       
+    }    
+
+    // Run the commands in the setup part of build.yml
+    async runSetup(setup_cmds,processor)
+    {               
+        for(let i=0; i< setup_cmds.length;i++)
+        {
+            let command = setup_cmds[i];
+            await this.sshIntoVM(command,processor);                
+        }
+    }
+
+    
+    async runClone(setup_cmds,processor){
+        await this.sshIntoVM(setup_cmds,processor);
+    }
+
+    // Run the commands for the specific job of build.yml
+    async runSteps(job_cmds,processor)
+    {      
+      for(let i=0; i< job_cmds.length;i++)
+      {
+          let command = job_cmds[i].run;            
+          await this.sshIntoVM(command,processor);                
+      }
+    }
+}
 
 exports.handler = async argv => {
-    const { job_name, build_yaml } = argv;
-    console.log(chalk.green(job_name, build_yaml));
-    await builder.runBuild(job_name, build_yaml);
+    const { job_name, build_yml,processor} = argv;
+    try 
+    {
+        let token = process.env.Git_Token
+        const doc = yaml.load(fs.readFileSync(build_yml, 'utf8'));                
+        let doc_json = JSON.parse(JSON.stringify(doc)); // Parsing build.yml file
+
+        var index = -1;
+        for(let i=0; i< doc_json.jobs.length;i++)
+        {
+            if(job_name == doc_json.jobs[i].name) // Determining the index of job to execute based on the job name passed in the command line.
+            {
+                index = i;
+            }
+        }
+        let setup_cmds = doc_json.setup;        
+        await new Setup().runSetup(setup_cmds,processor);
+
+        let clone_cmds = `git clone https://${token}@github.ncsu.edu/engr-csc326-staff/iTrust2-v10.git`
+
+        await new Setup().runClone(clone_cmds,processor);
+        if(index==-1){
+            return;
+        }
+
+        let job_cmds = doc_json.jobs[index].steps;           
+        await new Setup().runSteps(job_cmds,processor);
+    } 
+    catch (e) 
+    {
+        console.log(e);
+    }
+   
 };
--- commands/init.js
+++ commands/init.js
@@ -1,7 +1,11 @@
+const fs = require("fs");
 const chalk = require('chalk');
 const path = require('path');
-const builder = require('../lib/builder');
-
+const { exec } = require("child_process");
+const child_process = require("child_process");
+const execSync = require('child_process').execSync;
+const os = require('os');
+const { options } = require("yargs");
 exports.command = 'init';
 exports.desc = 'Prepare tool';
 exports.builder = yargs => {
@@ -9,10 +13,65 @@ exports.builder = yargs => {
     });
 };
 
-
 exports.handler = async argv => {
     const { processor } = argv;
+
     console.log(chalk.green("Preparing computing environment..."));
-    await builder.buildAndProvisionVM();
-    
+
+	if(processor == "Intel/Amd64")  // For Windows and Mac Intel processors
+    {
+        let pull_img = `bakerx pull focal cloud-images.ubuntu.com`;                   // Pull the focal image
+        child_process.execSync(pull_img, {stdio: ['inherit', 'inherit', 'inherit']});       
+
+        let createVM = `bakerx run pipeline-vm focal --ip 192.168.56.10 --sync --memory 4096`;  // Create VM
+        child_process.execSync(createVM, {stdio: ['inherit', 'inherit', 'inherit']});       
+	
+	let print_get_info = `bakerx ssh-info pipeline-vm --format json`;    // Display connection information of VM
+        child_process.execSync(print_get_info, {stdio: ['inherit', 'inherit', 'inherit']});    
+	    
+        let get_info = `bakerx ssh-info pipeline-vm --format json > "VM_Info.json"`;    // Store connection information of VM in JSON file
+        child_process.execSync(get_info, {stdio: ['inherit', 'inherit', 'inherit']});                                    
+    }
+    else // For Mac M1 processors
+    {
+
+        child_process.execSync(`cp "$HOME/Library/Application Support/basicvm/key" ./`);  
+        child_process.execSync(`mkdir keys`);
+        child_process.execSync(`mv key ./keys `);   // Retrieve ssh-key of basicvm
+        child_process.execSync(`vm pull`, {stdio: ['inherit', 'inherit', 'inherit']});   // Pull the image
+        child_process.execSync(`vm run pipeline-vm ubuntu:jammy`, {stdio: ['inherit', 'inherit', 'inherit']});
+        const info=child_process.execSync(`vm ssh-config pipeline-vm `);
+        var temp1 = {}
+        info.toString().trim().split(/\r?\n/).forEach(line =>  {
+            if(line.trim().length > 0) {
+                let l = line.trim().split(" ");
+                temp1[l[0]] = l[1];
+            }
+        });
+
+        temp1['Path'] = '~/Library/Application\ Support/basicvm/key';
+        temp1['port'] = 22;
+        temp1['identifyFile'] = path.join( path.dirname(require.main.filename), 'keys', 'key');
+
+        var jsonContent1 = JSON.stringify(temp1);   
+        const output = execSync('echo "VM Information"', { encoding: 'utf-8' });
+        console.log(output);
+        console.log(jsonContent1);  
+
+
+        // Creating JSON file and storing VM connection information
+        fs.writeFile("VM_Info.json", jsonContent1, 'utf8', function (err) {     
+            if (err) {
+                console.log("An error occured while writing JSON Object to File.");
+                return console.log(err);
+            }
+         
+        });
+
+
+
+    }
+
+ 
+   
 };
