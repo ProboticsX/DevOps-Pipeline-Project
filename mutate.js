f8131258fd822acd41e0e20e836cb7972c7a1131
sshubha <sshubha@ncsu.edu>
2022-04-13 20:47:16 -0400

    M2-modified

---
+++ mutate.js
@@ -0,0 +1,174 @@
+const esprima = require("esprima");
+const escodegen = require("escodegen");
+const options = {tokens:true, tolerant: true, loc: true, range: true };
+const fs = require("fs");
+//const chalk = require('chalk');
+
+let operations = [ NegateConditionals, ConditionalBoundaryMutations ]
+
+function rewrite( filepath, newPath ) {
+
+    var buf = fs.readFileSync(filepath, "utf8");
+    var ast = esprima.parse(buf, options);    
+
+    //let size = operations.length;
+
+    let op = operations[0];
+    
+    op(ast);
+
+    let code = escodegen.generate(ast);
+    fs.writeFileSync( newPath, code);
+}
+
+function ConditionalBoundaryMutations(ast){
+
+    let candidates = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "BinaryExpression" && node.operator === ">" ) {
+            candidates++;
+        }
+    })
+
+    let mutateTarget = getRandomInt(candidates);
+    let current = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "BinaryExpression" && node.operator === ">" ) {
+            if( current === mutateTarget ) {
+                node.operator = ">="
+                console.log( `Replacing > with >= on line ${node.loc.start.line}` );
+            }
+            current++;
+        }
+    })
+
+    candidates = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "BinaryExpression" && node.operator === "<" ) {
+            candidates++;
+        }
+    })
+
+    mutateTarget = getRandomInt(candidates);
+    current = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "BinaryExpression" && node.operator === "<" ) {
+            if( current === mutateTarget ) {
+                node.operator = "<="
+                console.log( `Replacing < with <= on line ${node.loc.start.line}` );
+            }
+            current++;
+        }
+    })
+
+}
+
+function NegateConditionals(ast) {
+
+    // let candidates = 0;
+    // traverseWithParents(ast, (node) => {
+    //     if( node.type === "BinaryExpression" && node.operator === "==" ) {
+    //         candidates++;
+    //     }
+    // })
+
+    // let mutateTarget = getRandomInt(candidates);
+    // let current = 0;
+    // traverseWithParents(ast, (node) => {
+    //     if( node.type === "BinaryExpression" && node.operator === "==" ) {
+    //         if( current === mutateTarget ) {
+    //             node.operator = "!="
+    //             console.log( `Replacing == with != on line ${node.loc.start.line}` );
+    //         }
+    //         current++;
+    //     }
+    // })
+
+    let candidates = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "BinaryExpression" && node.operator === "<" ) {
+            candidates++;
+        }
+    })
+
+    let mutateTarget = getRandomInt(candidates);
+    let current = 0;
+    traverseWithParents(ast, (node) => {
+        if( node.type === "BinaryExpression" && node.operator === "<" ) {
+            if( current === mutateTarget ) {
+                node.operator = ">"
+                console.log( `Replacing < with > on line ${node.loc.start.line}` );
+            }
+            current++;
+        }
+    })
+
+}
+
+rewrite("checkbox.io-micro-preview/marqdown.js", "checkbox.io-micro-preview/marqdown-mod.js")
+
+
+function getRandomInt(max) {
+    return Math.floor(Math.random() * max);
+}
+
+// A function following the Visitor pattern.
+// Annotates nodes with parent objects.
+function traverseWithParents(object, visitor)
+{
+    var key, child;
+
+    visitor.call(null, object);
+
+    for (key in object) {
+        if (object.hasOwnProperty(key)) {
+            child = object[key];
+            if (typeof child === 'object' && child !== null && key != 'parent') 
+            {
+            	child.parent = object;
+					traverseWithParents(child, visitor);
+            }
+        }
+    }
+}
+
+// Helper function for counting children of node.
+function childrenLength(node)
+{
+	var key, child;
+	var count = 0;
+	for (key in node) 
+	{
+		if (node.hasOwnProperty(key)) 
+		{
+			child = node[key];
+			if (typeof child === 'object' && child !== null && key != 'parent') 
+			{
+				count++;
+			}
+		}
+	}	
+	return count;
+}
+
+
+// Helper function for checking if a node is a "decision type node"
+function isDecision(node)
+{
+	if( node.type == 'IfStatement' || node.type == 'ForStatement' || node.type == 'WhileStatement' ||
+		 node.type == 'ForInStatement' || node.type == 'DoWhileStatement')
+	{
+		return true;
+	}
+	return false;
+}
+
+// Helper function for printing out function name.
+function functionName( node )
+{
+	if( node.id )
+	{
+		return node.id.name;
+	}
+	return "anon function @" + node.loc.start.line;
+}
