23f7aeebac9e3066fc736185a31e48170f2d6ff3
Jay <jshah7@ncsu.edu>
2022-05-04 20:42:45 -0400

    Final F0

---
+++ commands/test.js
@@ -0,0 +1,65 @@
+#!/usr/bin/env node
+const fs = require('fs');
+const yaml = require('js-yaml');
+const child_process = require("child_process");
+const execSync = require('child_process').execSync;
+const os = require('os');
+const { options } = require("yargs");
+
+const configFile = require("../lib/config");
+const mutationBuild = require("../lib/mutationBuild");
+
+const angularJest = require("../")
+
+require('dotenv').config();
+
+exports.command = 'test <job_name> <build_yml>';
+
+exports.handler = async argv => {
+    const { job_name, build_yml} = argv;
+    try 
+    {
+        
+        //reading the build.yml file
+        let buildYamlFile = await configFile.readBuildYaml(build_yml);
+
+        //running setup
+
+       // await configFile.runSetupSteps(buildYamlFile.setup);
+
+
+        // Determining the index of job to execute based on the job name passed in the command line.
+        var index = -1;
+        for(let i=0; i< buildYamlFile.jobs.length;i++)
+        {
+            if(job_name == buildYamlFile.jobs[i].name) 
+            {
+                index = i;
+            }
+        }
+        if(index==-1){
+            return;
+        }
+
+
+
+        if(job_name=="F0_test"){
+
+            let runSteps = buildYamlFile.jobs[index].steps;
+            await configFile.runSteps(runSteps);
+        }
+
+        if(job_name=="vehicle-test"){
+
+            let runSteps = buildYamlFile.jobs[index].steps;
+            await configFile.runSteps(runSteps);
+        }
+    } 
+    catch (e) 
+    {
+        console.log(e);
+    }
+   
+};
+
+
